9cbbed873ac5fbae1e09263043e74fd81ce85401
Joshua Ball <joshua.g.s.ball@gmail.com>
2015-01-29 12:39:45 -0800

    Squashed 'local_subtree_modules/test_private_hapi_plugin/' content from commit 024d118
    git-subtree-dir: local_subtree_modules/test_private_hapi_plugin
    git-subtree-split: 024d118dc32c85ec22388f06981b19805b7d20b9

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        bump: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['package.json'],
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'origin',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+                globalReplace: false
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-bump');
+
+};
